040ee5c508a74492ef675f97bc049a0aabdccecf
JinsuJinsu <hjs928@naver.com>
2022-01-13 11:24:02 +0900

    emaillist01 finish

--- emaillist01/src/main/java/com/poscoict/emaillist/dao/EmaillistDao.java
+++ emaillist01/src/main/java/com/poscoict/emaillist/dao/EmaillistDao.java
@@ -78,6 +78,61 @@ public class EmaillistDao {
 		return result;
 	
 }
+
+	public boolean insert(EmaillistVo vo) {
+		// TODO Auto-generated method stub
+		boolean result=false;
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// 1. JDBC 드라이버 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 2. 연결하기
+			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			String user = "webdb";
+			String passwd = "webdb";
+			conn = DriverManager.getConnection(url, user, passwd);
+			
+			// 3. SQL 준비
+			String sql = "insert into emaillist values(null, ?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. 바인딩(binding)
+			pstmt.setString(1,vo.getFirstName());
+			pstmt.setString(2,vo.getLastName());
+			pstmt.setString(3,vo.getEmail());
+			
+			// 5. SQL 실행
+			int count = pstmt.executeUpdate(); //insert가 된 수를 말한다.
+			result = count==1;
+		}
+		catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패 : " + e.getMessage());
+		}
+		catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패 : " + e.getMessage());
+		}
+		finally {
+			// 자원 정리
+			try {
+				if(rs!=null) {
+					rs.close();
+				}
+				if(pstmt!=null) {
+					pstmt.close();
+				}
+				if(conn!=null) {
+					conn.close();
+				}	
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}	
+		}
+		return result;
+	}
 }
 
 
--- emaillist01/src/main/webapp/index.jsp
+++ emaillist01/src/main/webapp/index.jsp
@@ -1,22 +1,48 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %>
+<%@ page import="com.poscoict.emaillist.dao.EmaillistDao" %>
+<%@ page import="com.poscoict.emaillist.vo.EmaillistVo" %>
 <!DOCTYPE html>
 <html>
+<%
+EmaillistDao dao = new EmaillistDao();
+List<EmaillistVo> list = dao.findAll();
+
+%>
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>방명록</title>
+<title>Insert title here</title>
 </head>
 <body>
-	<form method="post" action="">
-	<input type='hidden' name="id" value="">
-	<table>
+	<h1>메일 리스트에 가입되었습니다.</h1>
+	<p>입력한 정보 내역입니다.</p>
+	<!-- 메일정보 리스트 -->
+	<%
+		for(EmaillistVo vo : list){
+	%>
+	<table border="1" cellpadding="5" cellspacing="2">
 		<tr>
-			<td>비밀번호</td>
-			<td><input type="password" name="password"></td>
-			<td><input type="submit" value="확인"></td>
-			<td><a href="">메인으로 돌아가기</a></td>
+			<td align=right>First name: </td>
+			<td><%= vo.getFirstName() %></td>
+		</tr>
+		<tr>
+			<td align=right width="110">Last name: </td>
+			<td width="110"><%= vo.getLastName() %></td>
+		</tr>
+		<tr>
+			<td align=right>Email address: </td>
+			<td><%= vo.getEmail() %></td>
 		</tr>
 	</table>
-	</form>
+	<br>
+	<%
+		}
+	%>
+	<p>
+	<a href="/emaillist01/form.jsp">추가메일 등록</a>
+	</p>
+	<br>
 </body>
 </html>
